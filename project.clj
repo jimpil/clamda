595022402a7fb7d4badfa305ed7fe63f93cd27ac
Dimitrios Piliouras <jimpil1985@gmail.com>
2018-08-09 00:58:43 +0100

    deprecate repo

--- project.clj
+++ project.clj
@@ -1,8 +1,10 @@
-(defproject clamda "0.1.3-SNAPSHOT"
-  :description "FIXME: write description"
+(defproject clamda "0.1.3"
+  :description "DEPRECATED: see https://github.com/jimpil/clambda instead"
   :url "https://github.com/jimpil/clamda"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]]
   ;; exact match of the test dictionary
   :jar-exclusions [#"ddict\.txt"])
+
+
